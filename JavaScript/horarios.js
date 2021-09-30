cbfbc54272f48b2759e9e4f6da4d07048bc15d0d
Papalarva <papalarva1@gmail.com>
2021-09-29 20:38:41 -0600

    Se agrega el proyecto

---
+++ JavaScript/horarios.js
@@ -0,0 +1,143 @@
+let contador = 0;
+function opciones () {
+	contador++;
+	if (contador%2 == 0) {
+		var ocultar = document.getElementById('boton').nextElementSibling;
+		ocultar.style.cssText = 'display: none';
+	} else {
+		var mostrar = document.getElementById('boton').nextElementSibling;
+		mostrar.style.cssText = 'display: block';
+	}
+}
+let contador_2 = 0;
+function opciones_2() {
+	contador_2++;
+	if (contador_2%2 == 0) {
+		var ocultar_2 = document.getElementById('boton_2').nextElementSibling;
+		ocultar_2.style.cssText = 'display: none';
+	} else {
+		var mostrar_2 = document.getElementById('boton_2').nextElementSibling;
+		mostrar_2.style.cssText = 'display: block';
+	}
+}
+let contador_3 = 0;
+function opciones_3() {
+	contador_3++;
+	if (contador_3%2 == 0) {
+		var ocultar_3 = document.getElementById('boton_3').nextElementSibling;
+		ocultar_3.style.cssText = 'display: none';
+	} else {
+		var mostrar_3 = document.getElementById('boton_3').nextElementSibling;
+		mostrar_3.style.cssText = 'display: block';
+	}
+}
+let contador_4 = 0;
+function opciones_4() {
+	contador_4++;
+	if (contador_4%2 == 0) {
+		var ocultar_4 = document.getElementById('boton_4').nextElementSibling;
+		ocultar_4.style.cssText = 'display: none';
+	} else {
+		var mostrar_4 = document.getElementById('boton_4').nextElementSibling;
+		mostrar_4.style.cssText = 'display: block';
+	}
+}
+let contador_5 = 0;
+function opciones_5() {
+	contador_5++;
+	if (contador_5%2 == 0) {
+		var ocultar_5 = document.getElementById('boton_5').nextElementSibling;
+		ocultar_5.style.cssText = 'display: none';
+	} else {
+		var mostrar_5 = document.getElementById('boton_5').nextElementSibling;
+		mostrar_5.style.cssText = 'display: block';
+	}
+}
+let contador_6 = 0;
+function opciones_6() {
+	contador_6++;
+	if (contador_6%2 == 0) {
+		var ocultar_6 = document.getElementById('boton_6').nextElementSibling;
+		ocultar_6.style.cssText = 'display: none';
+	} else {
+		var mostrar_6 = document.getElementById('boton_6').nextElementSibling;
+		mostrar_6.style.cssText = 'display: block';
+	}
+}
+let contador_7 = 0;
+function opciones_7() {
+	contador_7++;
+	if (contador_7%2 == 0) {
+		var ocultar_7 = document.getElementById('boton_7').nextElementSibling;
+		ocultar_7.style.cssText = 'display: none';
+	} else {
+		var mostrar_7 = document.getElementById('boton_7').nextElementSibling;
+		mostrar_7.style.cssText = 'display: block';
+	}
+}
+let contador_8 = 0;
+function opciones_8() {
+	contador_8++;
+	if (contador_8%2 == 0) {
+		var ocultar_8 = document.getElementById('boton_8').nextElementSibling;
+		ocultar_8.style.cssText = 'display: none';
+	} else {
+		var mostrar_8 = document.getElementById('boton_8').nextElementSibling;
+		mostrar_8.style.cssText = 'display: block';
+	}
+}
+let contador_9 = 0;
+function opciones_9() {
+	contador_9++;
+	if (contador_9%2 == 0) {
+		var ocultar_9 = document.getElementById('boton_9').nextElementSibling;
+		ocultar_9.style.cssText = 'display: none';
+	} else {
+		var mostrar_9 = document.getElementById('boton_9').nextElementSibling;
+		mostrar_9.style.cssText = 'display: block';
+	}
+}
+let contador_10 = 0;
+function opciones_10() {
+	contador_10++;
+	if (contador_10%2 == 0) {
+		var ocultar_10 = document.getElementById('boton_10').nextElementSibling;
+		ocultar_10.style.cssText = 'display: none';
+	} else {
+		var mostrar_10 = document.getElementById('boton_10').nextElementSibling;
+		mostrar_10.style.cssText = 'display: block';
+	}
+}
+let contador_11 = 0;
+function opciones_11() {
+	contador_11++;
+	if (contador_11%2 == 0) {
+		var ocultar_11 = document.getElementById('boton_11').nextElementSibling;
+		ocultar_11.style.cssText = 'display: none';
+	} else {
+		var mostrar_11 = document.getElementById('boton_11').nextElementSibling;
+		mostrar_11.style.cssText = 'display: block';
+	}
+}
+let contador_12 = 0;
+function opciones_12() {
+	contador_12++;
+	if (contador_12%2 == 0) {
+		var ocultar_12 = document.getElementById('boton_12').nextElementSibling;
+		ocultar_12.style.cssText = 'display: none';
+	} else {
+		var mostrar_12 = document.getElementById('boton_12').nextElementSibling;
+		mostrar_12.style.cssText = 'display: block';
+	}
+}
+let contador_13 = 0;
+function opciones_13() {
+	contador_13++;
+	if (contador_13%2 == 0) {
+		var ocultar_13 = document.getElementById('boton_13').nextElementSibling;
+		ocultar_13.style.cssText = 'display: none';
+	} else {
+		var mostrar_13 = document.getElementById('boton_13').nextElementSibling;
+		mostrar_13.style.cssText = 'display: block';
+	}
+}
